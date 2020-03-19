a08bdcbdfc84d780ef5b804a99e5efa40e49acd7
Sirllon <61039342+SIRLLON@users.noreply.github.com>
2020-03-19 01:13:37 -0300

    exercicos
    da fundacao bradesco

--- JS/aula ed.js
+++ JS/aula ed.js
@@ -1,27 +1,41 @@
-window.alert('bem-vindo a camisaria educafro');
+window.alert('bem-vindo ao exercicios do ED da educafrotech');
 let vnome = window.prompt('Digite o nome:');
-let idade = (prompt("Qual é a sua idade?"));
+let data = (prompt("Qual é a sua data de nascimento?"));
+var d = new Date()
+let nasci = d.getFullYear() - data;
 let genero = prompt("Qual é o seu gênero? Digite 1 para feminino. Digite 2 para masculino.");
-if (idade < 28) {
+if (nasci <= 10) {
     if (genero == '1') {
-        document.write(`Seja bem-vinda, ${vnome} ! Você é ${28 - parseInt(idade)} anos mais jovem do que Sirllon.`);
+        document.write(`Seja bem-vinda, ${vnome} ! Você é criança.`);
     } else if (genero == "2") {
-        document.write(`Seja bem-vindo, ${vnome} ! Você é ${28 - parseInt(idade)} anos mais jovem do que Sirllon.`);
+        document.write(`Seja bem-vindo, ${vnome} ! Você é criança.`);
     }
     /*else if (generousu == "3") 
     {
-        document.write(`Olá, ${nomeusu} ! Seu gênero é outros  e em relação a mim, Juliana, você é ${(idadeju - idadeusu)} anos mais jovem.`);
+        document.write(`Olá, ${nomeusu} ! Seu gênero é outros  e em relação a mim, Juliana, você é ${(nasciju - nasciusu)} anos mais jovem.`);
     }*/
-} else if (idade > 28) {
+} else if (nasci <= 16) {
     if (genero == '1') {
-        document.write(`Seja bem-vinda, ${vnome} ! Você é ${parseInt(idade)-28} anos mais velho do que Sirllon.`);
+        document.write(`Seja bem-vinda, ${vnome} ! Você é adolescente.`);
     } else if (genero == "2") {
-        document.write(`Seja bem-vindo, ${vnome} ! Você é ${parseInt(idade)-28} anos mais velho do que Sirllon.`);
+        document.write(`Seja bem-vindo, ${vnome} ! Você é adolescente.`);
     }
-} else if (idade == 28) {
+} else if (nasci <= 28) {
     if (genero == '1') {
-        document.write(`Seja bem-vinda, ${vnome} ! Tem a mesma idade que o Sirllon.`);
+        document.write(`Seja bem-vinda, ${vnome} ! Você é jovem.`);
     } else if (genero == "2") {
-        document.write(`Seja bem-vindo, ${vnome} ! Tem a mesma idade que o Sirllon.`);
+        document.write(`Seja bem-vindo, ${vnome} !Você é jovem.`);
+    }
+} else if (nasci <= 55) {
+    if (genero == '1') {
+        document.write(`Seja bem-vinda, ${vnome} ! Você é maduro.`);
+    } else if (genero == "2") {
+        document.write(`Seja bem-vindo, ${vnome} !Você é maduro.`);
+    }
+} else if (nasci > 55) {
+    if (genero == '1') {
+        document.write(`Seja bem-vinda, ${vnome} ! Você é idoso.`);
+    } else if (genero == "2") {
+        document.write(`Seja bem-vindo, ${vnome} !Você é idoso.`);
     }
 }
